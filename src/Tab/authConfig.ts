f142225683680972d5d0fd019cc8b159b0923307
Brigitte HOUSSA SAVA <hsb@iokeo.com>
2026-04-29 16:03:16 +0100

    changement de l'URI

--- src/Tab/authConfig.ts
+++ src/Tab/authConfig.ts
@@ -2,7 +2,7 @@ export const msalConfig = {
   auth: {
     clientId: "aafa68ec-013e-4d1a-8a4e-ba47fe93e545",
     authority: "https://login.microsoftonline.com/4431d0bb-7e97-4ac6-8531-e2727c16fbc8",
-    redirectUri: "https://test-deploiement-sans-azure.onrender.com",
+    redirectUri: "https://test-deploiement-sans-azure.onrender.com/tabs/home",
   },
   cache: {
     cacheLocation: "sessionStorage",
